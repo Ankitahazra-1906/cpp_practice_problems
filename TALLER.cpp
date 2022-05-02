e7d8ff9ba0eaefa4852b62debf153a1432fc6dd5
Ankita Hazra <ankitahazra2000@gmail.com>
2022-05-02 23:36:19 +0530

    Add files via upload

---
+++ TALLER.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t,x,y;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        cin>>x>>y;
+        if(x>y){
+            cout<<"A"<<endl;
+        }
+        else{
+            cout<<"B"<<endl;
+        }
+    }
+}
+	// your code goes here
+
+
