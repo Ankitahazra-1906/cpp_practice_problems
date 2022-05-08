f4d8a47d5f1329899630325be5dcb9464672344a
Ankita Hazra <ankitahazra2000@gmail.com>
2022-05-08 12:28:51 +0530

    Add files via upload

---
+++ codechef_problems/BIRDFARM.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,x,y,z;
+	cin>>t;
+	while(t--){
+	    cin>>x>>y>>z;
+	    if(z%x==0 && z%y==0)
+            cout<<"any"<<endl;
+	    else if(z%x==0 && z%y!=0)
+	        cout<<"chicken"<<endl;
+	    else if(z%y==0 && z%x!=0)
+	        cout<<"duck"<<endl;
+        
+        else
+            cout<<"none"<<endl;
+	    
+	}
+	return 0;
+}
---
+++ codechef_problems/PRESENTS.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,n;
+	cin>>t;
+	while(t--){
+	    cin>>n;
+	    cout<<n-n/5<<endl;
+	}
+	return 0;
+}
